f57b37d64cb177579d6a014ea7a6f6c59c63899f
Lancelcode <127974834+Lancelcode@users.noreply.github.com>
2023-05-03 16:49:32 +0100

    JS-var,let,const

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,32 +1,32 @@
 //Lo que ocurre cuando usamos var es que el compilador de JS , crea una variable  en  "var"
-//y luego le assigna el nombre miVariable, luego a le assignamos con "=" el valor de 'variable'.
+//y luego le assigna el nombre -miJamoncito-, luego le assignamos con "=" el valor de 'saborcitoRico'.
 
 //EJEMPLO 1 VAR funciona correctamente
-/*
-var miVariable
 
-miVariable = 'variable'
+var miJamoncito = 'saborcitoRico'
+miJamoncito = 'otro valor saborcito'//<----- aqui actualiza el valor , ahora printeara 'otro valor saborcito'
+//console.log(miJamoncito) //printeara la palabra 'saborcitoRico'
 
-console.log(miVariable) //printeara la palabra 'variable'
-*/
 
 //EJEMPLO 2 VAR (A & B) no funciona correctamente
-/*
+
 //Ejemplo A
-var miVariable
-console.log(miVariable) //no a sido declarada, printeara 'undefined' ya que no esta declarada aun, en otros lenguajes daria error.
-miVariable = 'variable' //aqui seria declarada como: 'variable'
------------------------------------------------------------------------------------------------------------------------------------
+/*
+var miJamoncito
+console.log(miJamoncito) //no a sido declarada, printeara 'undefined' ya que no esta declarada aun, en otros lenguajes daria error.
+miJamoncito = 'saborcitoRico' //aqui seria declarada como: 'saborcitoRico'
 */
+
+/*
 //Ejemplo B
-console.log(miVariable) //no a sido declarada, printeara 'undefined' ya que no esta declarada
-var miVariable //<--- variable vacia sin valor declarado.
-miVariable = 'variable' //aqui seria declarada como: 'variable'.
+console.log(miJamoncito) //no a sido declarada, printeara 'undefined' ya que no esta declarada
+var miJamoncito //<--- saborcitoRico vacia sin valor declarado.
+miJamoncito = 'saborcitoRico' //aqui seria declarada como: 'saborcitoRico'.
+*/
 
-
-//let variable = 'variable creada con let'
-
-
-
+let saborcitoRico = 'saborcitoRico creada con let'
+saborcitoRico = 'otro valor a let'
 
 //const constante = 'este valor es constante'
+
+console.log(miJamoncito,saborcitoRico)
